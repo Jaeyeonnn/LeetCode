f685378055aed59308be766ffbc6904c781e54e3
Jaeyeon Lee <162037788+Jaeyeonnn@users.noreply.github.com>
2025-08-19 11:33:07 -0700

    Time: 555 ms (62.62%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -2,14 +2,10 @@
 
 
 
+select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) as fraction
+from Activity
+where (player_id,DATE_SUB(event_date,INTERVAL 1 DAY))
+in (select player_id, min(event_date) as first_login from Activity group by player_id )
 
-select
-round(avg(case when a2.player_id is not null then 1 else 0 end), 2) as fraction
-from activity a1
-left join activity a2
-on a1.player_id = a2.player_id
-and a1.event_date = date_sub(a2.event_date, interval 1 day)
-where (a1.player_id, a1.event_date) in (select player_id, min(event_date) from activity
-group by player_id)
 
 
